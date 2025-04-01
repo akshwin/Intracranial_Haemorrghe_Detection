af98d24d082ba4b1ea4f9ffe09046216da8e6042
akshwin tamilselvan <akshwint.2003@gmail.com>
2025-04-02 00:23:31 +0530

    added model

--- app.py
+++ app.py
@@ -1,47 +1,61 @@
 import streamlit as st 
 from PIL import Image
-from tensorflow.keras.utils import load_img,img_to_array
+from tensorflow.keras.utils import load_img, img_to_array
 import numpy as np 
 from keras.models import load_model 
+import os
 
-model = load_model ("tuberculosis.h5")
-labels ={0:'No Tuberculosis',1:'Tuberculosis'}
-tuberculosis = {'Tuberculosis'}
+# Load Model
+model = load_model("ich.h5")
+labels = {0: 'No ICH', 1: 'ICH'}
+ich = {'ICH'}
 
+# Ensure Upload Directory Exists
+UPLOAD_FOLDER = "./upload_image"
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+# Image Processing Function
 def processed_img(img_path):
-    img=load_img(img_path,target_size=(224,224,3))
-    img=img_to_array(img)
-    img=img/255
-    img=np.expand_dims(img,[0])
-    answer=model.predict(img)
-    y_class = answer.argmax(axis=-1)
-    y = " ".join(str(x) for x in y_class)
-    y = int(y)
-    res = labels[y]
+    img = load_img(img_path, target_size=(224, 224))  # Removed third dimension
+    img = img_to_array(img)
+    img = img / 255.0  # Normalize the image
+    img = np.expand_dims(img, axis=0)  # Expand dimensions correctly
+    
+    answer = model.predict(img)
+    y_class = answer.argmax(axis=-1)[0]  # Get the class index
+    res = labels[y_class]  # Map index to label
+    
     return res.capitalize()
 
+# Streamlit UI
 def run():
-    st.title(" Tuberculosis Detector 🫁")
-    st.subheader("Upload the X-Ray Image:")
-    
-    st.sidebar.header("About the projet :")
-    st.sidebar.write("📌 The project is developed using a Hybrid CNN LSTM Architecture named TBXNet Pro")
-    st.sidebar.write("📌 The model classifies whether the X-Ray iamge uploaded had Tuberculosis or not.")
-    st.sidebar.write("📌 The model acheived an accuracy of 98 percent.")
-    
-    img_file = st.file_uploader("Choose an image",type=['jpg','jpeg','png'])
+    st.title("Intracranial Hemorrhage Detection 🧠")
+    st.subheader("Upload the MRI Image:")
 
-    if img_file is not None :
-        img  = Image.open(img_file).resize((250,250))
+    st.sidebar.header("About the project:")
+    st.sidebar.write("📌 The project is developed using a Convolutional Neural Network with an Attention mechanism.")
+    st.sidebar.write("📌 The model detects whether the patient has Intracranial Hemorrhage or not.")
+    st.sidebar.write("📌 The model achieved an accuracy of 92 percent.")
+
+    img_file = st.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
+
+    if img_file:
+        img = Image.open(img_file).resize((250, 250))
         st.image(img)
-        save_image_path = './upload_image/'+img_file.name
-        with open(save_image_path,"wb") as f:
+
+        save_image_path = os.path.join(UPLOAD_FOLDER, img_file.name)
+        
+        with open(save_image_path, "wb") as f:
             f.write(img_file.getbuffer())
 
-        if img_file is not None :
-                result = processed_img(save_image_path)
-                if result in tuberculosis :
-                    st.error('**TUBERCULOSIS DETECTED!!**')
-                else :
-                    st.success('**NO TUBERCULOSIS!!**')
-run()
+        result = processed_img(save_image_path)
+
+        if result in ich:
+            st.error('**ICH DETECTED!!**')
+        else:
+            st.success('**NO ICH!!**')
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    run()
