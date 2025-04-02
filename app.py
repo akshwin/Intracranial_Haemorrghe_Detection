773b3f47ed3aea7c0e4e8ee5a70b88555e6b908e
akshwin tamilselvan <akshwint.2003@gmail.com>
2025-04-02 23:01:28 +0530

    added model

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
 import streamlit as st 
 from PIL import Image
-from tensorflow.keras.utils import load_img, img_to_array
+from tensorflow.keras.utils import load_img,img_to_array
 import numpy as np 
 from keras.models import load_model 
-import os
 
-# Load Model
-model = load_model("ich.h5")
-labels = {0: 'No ICH', 1: 'ICH'}
-ich = {'ICH'}
+model = load_model ("ich.h5")
+labels ={0:'No ICH',1:'ICH'}
+tuberculosis = {'ICH'}
 
-# Ensure Upload Directory Exists
-UPLOAD_FOLDER = "./upload_image"
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-# Image Processing Function
 def processed_img(img_path):
-    img = load_img(img_path, target_size=(224, 224))  # Removed third dimension
-    img = img_to_array(img)
-    img = img / 255.0  # Normalize the image
-    img = np.expand_dims(img, axis=0)  # Expand dimensions correctly
-    
-    answer = model.predict(img)
-    y_class = answer.argmax(axis=-1)[0]  # Get the class index
-    res = labels[y_class]  # Map index to label
-    
+    img=load_img(img_path,target_size=(224,224,3))
+    img=img_to_array(img)
+    img=img/255
+    img=np.expand_dims(img,[0])
+    answer=model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
     return res.capitalize()
 
-# Streamlit UI
 def run():
     st.title("Intracranial Hemorrhage Detection 🧠")
     st.subheader("Upload the MRI Image:")
@@ -37,25 +28,21 @@ def run():
     st.sidebar.write("📌 The project is developed using a Convolutional Neural Network with an Attention mechanism.")
     st.sidebar.write("📌 The model detects whether the patient has Intracranial Hemorrhage or not.")
     st.sidebar.write("📌 The model achieved an accuracy of 92 percent.")
+    
+    img_file = st.file_uploader("Choose an image",type=['jpg','jpeg','png'])
 
-    img_file = st.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
-
-    if img_file:
-        img = Image.open(img_file).resize((250, 250))
+    if img_file is not None :
+        img  = Image.open(img_file).resize((250,250))
         st.image(img)
-
-        save_image_path = os.path.join(UPLOAD_FOLDER, img_file.name)
-        
-        with open(save_image_path, "wb") as f:
+        save_image_path = './upload_image/'+img_file.name
+        with open(save_image_path,"wb") as f:
             f.write(img_file.getbuffer())
 
-        result = processed_img(save_image_path)
+        if img_file is not None :
+                result = processed_img(save_image_path)
+                if result in tuberculosis :
+                    st.error('**ICH DETECTED!!**')
+                else :
+                    st.success('**NO ICH!!**')
+run()
 
-        if result in ich:
-            st.error('**ICH DETECTED!!**')
-        else:
-            st.success('**NO ICH!!**')
-
-# Run the Streamlit app
-if __name__ == "__main__":
-    run()
